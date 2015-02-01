3ca51fb105bb4f654eda6cadee7405890f02b6b4
kollivier <kilian.ollivier@insa-lyon.fr>
2015-02-01 16:19:56 +0100

    Add test file

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for(int i = 0; i < 1000000; i++)
+	{
+		cout << "C C" << i << " " << i << " " << i << " " << 1 << endl;
+	}
+	cout << "EXIT" << endl;
+	return 0;
+}
